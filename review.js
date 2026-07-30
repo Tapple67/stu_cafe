87705a7cf3a67dd7ef0d6b20b18e399700a15f9b
내 GitHub 아이디 < GitHub >
2026-07-30 17:52:43 +0900

    0730_finish

--- review.js
+++ review.js
@@ -24,12 +24,12 @@ function reviewpage(){
     for ( i = 0 ; i < foods.length ; i++){
         let obj = foods[i]
         if (select == obj.foodCode){
-            html += `<div class="food-info">
-            <img class="food-image" src="음식사진/${foods[i].image}" alt="음식사진">
+            html += `<div class="review-food-info">
+            <img class="review-food-image" src="음식사진/${foods[i].image}" alt="음식사진">
 
-            <div class="food-content">
-                <h2 class="food-name">${foods[i].foodName}</h2>
-                <p class="food-price">${foodDetails[i].price}</p>
+            <div class="review-food-content">
+                <h2 class="review-food-name">${foods[i].foodName}</h2>
+                <p class="review-food-price">${foodDetails[i].price}</p>
             </div>
         </div>
         `
@@ -37,10 +37,65 @@ function reviewpage(){
     }
 
 
-    document.querySelector('.food-info').innerHTML = html
+    document.querySelector('.review-food-info').innerHTML = html
 
 }
 
+// 별점 저장하는 전역 변수
+let rating = 0;
+
+function selectStar(number){
+    rating = number;
+    const starstar = document.querySelectorAll('.review-star-box > .review-button');
+    for(i=0; i<starstar.length ;i++){
+        if(i<number){
+            starstar[i].innerHTML = '★'
+        }else{
+            starstar[i].innerHTML = '☆'
+        }
+    }
+    console.log(number)
+}
+
+
 function reviewupload(){
+
+    // 선택한 푸드 코드
+    const url = new URLSearchParams(location.search)
+    let foodCode = url.get('foodCode')
+
+
+    let categories = localStorage.getItem('categories')
+    if(categories == null){ categories=[]}
+    else{categories = JSON.parse(categories)}
+
+    let foods = localStorage.getItem('foods')
+    if(foods == null){ foods=[]}
+    else{foods = JSON.parse(foods)}
+
+    let foodDetails = localStorage.getItem('foodDetails')
+    if(foodDetails == null){ foodDetails=[]}
+    else{foodDetails = JSON.parse(foodDetails)}
+
+    let reviews = localStorage.getItem('reviews')
+    if(reviews == null){ reviews=[]}
+    else{reviews = JSON.parse(reviews)}
+
+    // html 에서 가져오기 
     
+    const content = document.querySelector('#reviewContent').value
+    
+    
+    
+    //리뷰목록에 푸쉬
+    
+    let reviewCode = reviews.length == 0 ? 1 : reviews.length + 1
+    let obj = { reviewCode , content , rating , foodCode }
+    
+    reviews.push(obj)
+    localStorage.setItem('reviews' , JSON.stringify(reviews))
+    alert('리뷰가 등록되었습니다.')
+
+    location.href="write.html"
+
 }
