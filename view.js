0a6f57dff14fe1650fd2340c1b8463d52e90c5fd
seungy07 <ksykim07@gmail.com>
2026-07-31 12:24:08 +0900

    코드 정리

--- view.js
+++ view.js
@@ -1,16 +1,3 @@
-/*
-    넣어야 할 기능
-추천점수를 보여드릴게요 부분에 추천점수 출력
-list에서 고른 목록의 사진을 출력 
- 선택한 메뉴 이름 출력
-
- 별점을 보고 몇점 이상(조건)이면 "별점수가 높아요" 출력
- 판매량을 보고 상위권이면 '판매량 상위권이예요' 출력
- 
-실습 13-1 페이지 참조 
-local storge 에 데이터가 있다는 가정
-*/
-
 // list -> 선택한 메뉴 -> 조회 ( 사진 이름 추천도점수 별점 판매량순위)
 
 // 전역변수로 현재 URL 상 foodCode 확인
@@ -24,8 +11,8 @@ if(foods==null){foods=[];} // 빈배열일 경우
 let select_value = 0
 function changeDay(select) {
     select_value = select.value
-    view_print() // 값이 바뀌면 다시 조회
-}
+    view_print() }// 값이 바뀌면 다시 조회
+
 //조회
 view_print() // 페이지 넘어올시 바로 실행
 function view_print(){
@@ -38,8 +25,7 @@ function view_print(){
             document.querySelector(".view_write").innerHTML =`<img src="학생식당_아이콘/07_리뷰쓰기.png" />
                                                                 <a href="write.html?foodCode=${obj.foodCode}">리뷰쓰기</a>`
             document.querySelector('.view_img > img').src = "음식사진/" + obj.image
-                             
-                                                                
+                                                              
             // 별점수에 따른 멘트 설정
             if(avgrating(foodcode) >= 3.2){
                 document.querySelector(".view_menu_reson1").innerHTML = "별점수가 높아요!" + avgrating(foodcode)
@@ -52,21 +38,17 @@ function view_print(){
             let top10Count = Math.ceil(sortedFoods.length * 0.1);
             // 현재 음식의 순위 찾기
             let rank = sortedFoods.findIndex(f => f.foodCode == foodcode) + 1;
-            
             // 상위 10% 여부
             if(rank <= top10Count){
                 document.querySelector(".view_menu_reson2").innerHTML = "판매량 상위 메뉴예요!"
-            }else{document.querySelector(".view_menu_reson2").innerHTML = "맛있어 보일 겁니다."}
-        }
-    }
-}
+            }else{document.querySelector(".view_menu_reson2").innerHTML = "맛있어 보일 겁니다."}}}}
+
 // 추천도 점수 계산하는 식
 function getScore(foodCode){
     let maxSales = 0  // 최고 판매량 찾기
     for(let i=0; i<foods.length;i++){
         if(foods[i].sales > maxSales){
             maxSales = foods[i].sales;}}
-
     let food;
     for (let i = 0; i < foods.length; i++) {
     if (foods[i].foodCode == foodCode) {
@@ -94,8 +76,7 @@ function getScore(foodCode){
         salesWeight = 30;
         ratingWeight = 30;
         favoriteWeight = 30;
-        newMenuWeight = 10;
-    }
+        newMenuWeight = 10;}
 
     // 판매량 점수 
     let salesScore = (food.sales / maxSales) * salesWeight;
@@ -108,14 +89,11 @@ function getScore(foodCode){
     for(let i=0; i<=foods.length-1;i++){
         if(foods[i].categoryCode == 4){
             newMenuScore = newMenuWeight;
-            break; 
-        }
-    } 
+            break; }} 
 
     // 총점
     let totalScore = salesScore + ratingScore + favoriteScore + newMenuScore;
-    return parseInt(totalScore);
-}
+    return parseInt(totalScore);}
 
 //평균 별점 계산
 function avgrating(foodCode){
@@ -127,10 +105,8 @@ function avgrating(foodCode){
     for(let i=0; i<=reviews.length-1;i++){
         if(reviews[i].foodCode == foodCode){ // 리뷰배열 foodCode 랑 URL foodcode 비교
             count += 1
-            ratingSum += reviews[i].rating
-        }
-    }
+            ratingSum += reviews[i].rating}}
+
     let avg = ratingSum / count
     if(count == 0){return 0;} // 평균 별점을 구하고 변수에 저장
-    return avg; // 평균 별점 값 반환
-}
+    return avg; } // 평균 별점 값 반환
