43bbe428f19c277ff858760850230bae468ba8a3
내 GitHub 아이디 < GitHub >
2026-08-06 16:42:28 +0900

    Jdb 서현욱

--- Jdb.sql
+++ Jdb.sql
@@ -4,3 +4,9 @@ CREATE DATABASE jdb;
 
 USE jdb;
 
+-- 카테고리 테이블 
+CREATE Table categories(
+    categoryCode INT AUTO_INCREMENT,
+    Constraint PRIMARY key (categoryCode),
+    categoryName varchar(100)
+) 
