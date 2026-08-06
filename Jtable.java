6ba1e81663ce6268200567ae57a6a99e8aa984ae
JUN <guswns020607@naver.com>
2026-08-06 12:59:55 +0900

    JUN

---
+++ Jtable.java
@@ -0,0 +1,17 @@
+
+class Food{
+    int foodCode;
+    String foodName;
+    int sale;
+    String image;
+    int categoryCode;
+}
+
+public class Jtable {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
