34eed9aad145b398c880c938d2641b2d5f66846d
JUN <guswns020607@naver.com>
2026-08-06 16:43:28 +0900

    feat foods

--- Jdb.sql
+++ Jdb.sql
@@ -4,3 +4,14 @@ CREATE DATABASE jdb;
 
 USE jdb;
 
+
+
+CREATE Table foods(
+    foodCode int,
+    constraint PRIMARY KEY (foodCode),
+    foodName VARCHAR(20) NOT NULL,
+    sales INT UNSIGNED,
+    image VARCHAR(20),
+    categoryCode INT,
+    constraint Foreign Key (categoryCode) REFERENCES categories(categoryCode)
+)
