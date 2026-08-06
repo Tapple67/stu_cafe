45ee410f3aebdb54464281cf84a8951f678a6dca
rngustmd <hsk1485@gmail.com>
2026-08-06 12:58:26 +0900

    FoodDetail class

--- Jtable.java
+++ Jtable.java
@@ -1,6 +1,27 @@
 
 
 public class Jtable {
+    public static void main(String[] args) {
+        
+    }
     
     
 }
+class FoodDetail {
+    int foodCode;      
+    int price;     
+    int calories;      
+    double protein;    
+    double fat;        
+    int carbs;
+    FoodDetail(){}
+
+    FoodDetail(int foodCode, int price, int calories, double protein, double fat, int carbs) {
+        this.foodCode = foodCode;
+        this.price = price;
+        this.calories = calories;
+        this.protein = protein;
+        this.fat = fat;
+        this.carbs = carbs;         
+    } // FoodDetail end
+} // class end
