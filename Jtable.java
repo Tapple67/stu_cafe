8588285bbec351519ecd135e4237674338be08a6
JUN <guswns020607@naver.com>
2026-08-06 13:01:09 +0900

    JUN

--- Jtable.java
+++ Jtable.java
@@ -5,6 +5,16 @@ class Food{
     int sale;
     String image;
     int categoryCode;
+    
+    public Food(int foodCode, String foodName, int sale, String image, int categoryCode) {
+        this.foodCode = foodCode;
+        this.foodName = foodName;
+        this.sale = sale;
+        this.image = image;
+        this.categoryCode = categoryCode;
+    }
+
+    
 }
 
 public class Jtable {
