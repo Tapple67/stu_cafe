0f6261ce6be91b4b53426da2308f87d43c29ad46
JUN <guswns020607@naver.com>
2026-08-06 13:08:56 +0900

    취합

--- Jtable.java
+++ Jtable.java
@@ -2,21 +2,23 @@ public class Jtable {
     public static void main(String[] args) {
         
     }
-<<<<<<< HEAD
 }
 
 class Categories{
     int categoryCode;
     String categoryName;
-
+    public Categories(int categoryCode, String categoryName) {
+            this.categoryCode = categoryCode;
+            this.categoryName = categoryName;
+        }
+    
+}
 
 class Review{
     int reviewCode;
     String content; 
     int rating; 
     int foodCode;
-=======
->>>>>>> 45ee410f3aebdb54464281cf84a8951f678a6dca
     
     public Review(int reviewCode, String content, int rating, int foodCode) {
         this.reviewCode = reviewCode;
@@ -24,16 +26,6 @@ class Review{
         this.rating = rating;
         this.foodCode = foodCode;
     }
-    
-}
-<<<<<<< HEAD
-
-    
-    public Categories(int categoryCode, String categoryName) {
-        this.categoryCode = categoryCode;
-        this.categoryName = categoryName;
-    }
-    
 }
 
 class Food{
@@ -51,13 +43,8 @@ class Food{
         this.categoryCode = categoryCode;
     }
 
-    
 }
 
-
-
-
-=======
 class FoodDetail {
     int foodCode;      
     int price;     
@@ -76,4 +63,3 @@ class FoodDetail {
         this.carbs = carbs;         
     } // FoodDetail end
 } // class end
->>>>>>> 45ee410f3aebdb54464281cf84a8951f678a6dca
